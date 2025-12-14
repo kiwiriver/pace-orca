f5149cf277546785849b7cc652b61865e293a9c7
Meng Gao <meng.gao@nasa.gov>
2025-12-14 20:41:05 +0000

    add a file to create symbolic link

---
+++ test/link.sh
@@ -0,0 +1,3 @@
+#create symbolic link here
+ln -s /mnt/mfs/mgao1/analysis/github/pace-orca/scripts/run_orca_rapid.sbatch run_rapid
+ln -s /mnt/mfs/mgao1/analysis/github/pace-orca/scripts/run_orca_spotlight.sbatch run_spot
